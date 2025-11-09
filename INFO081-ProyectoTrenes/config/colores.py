0a8cdea8963f5be46f060c7f33a8b8a0dc765252
edwszn <eduardopezo31tt@gmail.com>
2025-11-09 15:47:03 -0300

    agregue algunos colores

---
+++ INFO081-ProyectoTrenes/config/colores.py
@@ -0,0 +1,8 @@
+#algunos colores que tiene relación con el tema de los ferroviarios, luego podemos ir viendo q tal queda
+COLOR_FONDO= "#1F2A32" #azul petroleo
+COLOR_PANEL= "#2E3C46" #gris acero
+COLOR_TEXTO= "#E0E0E0" #gris claro
+COLOR_BOTON= "#FFB703" #amarillo
+COLOR_BOTON_HOVER= "#E09E02" #amarillo mas oscuro
+COLOR_ALERTA= "#F34654" #rojo
+COLOR_EXITO= "#0E8D4B"  #verdecito
